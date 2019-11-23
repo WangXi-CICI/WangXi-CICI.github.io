96826d6ebb017e6b8fa68307fcf16113a1a7bd94
WangXi-CICI <526164068@qq.com>
2019-11-23 14:15:11 +0800

    Update li_ning.js

--- final/li_ning.js
+++ final/li_ning.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
            var chart = c3.generate({
             data: {
                 columns: dataSet,
-                type : 'line'
+                type : 'bar'
             },
             axis: {
               x: {label: 'Product'},
