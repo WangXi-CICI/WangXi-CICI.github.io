a1d33ec72bb856be4ceec1aa10374d6d6287d2ce
WangXi-CICI <526164068@qq.com>
2019-11-28 16:33:17 +0800

    update

--- final project /chart.js
+++ final project /chart.js
@@ -1,64 +1,14 @@
-$(document).ready(function(){
 
-    $("button#hide_h2").click(function() {
-        $("h2").hide(500);
-    });
 
-    $("button#show_h2").click(function() {
-        $("h2").show(300);
-        $("h2").css("color","blue");
-        $("h2").html("You clicked me hard.");
-    });
-
-    $("button#clear_screen").click(function() {
-        var $x = $("container");
-        $x.empty();
-    });
-
-   
-
-     $("button#get_data2").click(function() {
-      var items = [];
-      var i = 0;
-      var airtable_read_endpoint = "https://api.airtable.com/v0/app9T6xQJ88UdG60q/Table%203?api_key=keysvB4lHDB6fjPJK";
-      var dataSet = [];
-      $.getJSON(airtable_read_endpoint, function(result) {
-             $.each(result.records, function(key,value) {
-                 items = [];
-                     items.push(value.fields.year);
-                     items.push(value.fields.percentage);
-                     dataSet.push(items);
-                     console.log(items);
-              }); // end .each
-              console.log(dataSet);
-
-           $('#table2').DataTable( {
-               data: dataSet,
-               retrieve: true,
-               columns: [
-                   { title: "year",
-                     defaultContent:""},
-                   { title: "percentage",
-                       defaultContent:"" },
-               ]
-           } );
-
-           var chart = c3.generate({
-                data: {
-                    columns: dataSet,
-                    type : 'bar'
-                },
-                axis: {
-                  x: {label: 'year'},
-                  y: {label: 'percentege'}
-                },
-                bar: {
-                    title: "# of Items by Product Category:",
-                }
-            });
-
-      }); // end .getJSON
-
-   }); // end button
-
-}); // document ready
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['中国品牌', 38],
+            ['境外品牌', 62],
+        ],
+        type : 'donut'
+    },
+    donut: {
+        title: "Dogs love:",
+    }
+});
