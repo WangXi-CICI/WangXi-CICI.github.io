88ea4c13e8f9957769dd9cb30074a0f32cec6a92
WangXi-CICI <526164068@qq.com>
2019-11-28 17:34:11 +0800

    update

--- final project /js/chart.js
+++ final project /js/chart.js
@@ -1,32 +1,63 @@
-var chart = Highcharts.chart('chart1',{
-	chart: {
-		type: 'column'
-	},
-	title: {
-		text: 'MAT2017-MAT2019国潮品牌渗透率趋势'
-	},
-	subtitle: {
-		text: '数据来源: 有货电商平台'
-	},
-	xAxis: {
-		categories: [
-			'MAT2017','MAT2018','MAT2019'
-		],
-		crosshair: true
-	},
-	yAxis: {
-		min: 0,
-		title: {
-			text: '渗透率'
-		}
-	},
-	plotOptions: {
-		column: {
-			borderWidth: 0
-		}
-	},
-	series: [{
-		
-		data: [25%,32%,38%]
-	},]
-});
+$(document).ready(function(){
+
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color","blue");
+        $("h2").html("You clicked me hard.");
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
+
+     $("button#get_data2").click(function() {
+      var items = [];
+      var i = 0;
+      var airtable_read_endpoint = "https://api.airtable.com/v0/app9T6xQJ88UdG60q/Table%203?api_key=keysvB4lHDB6fjPJK";
+      var dataSet = [];
+      $.getJSON(airtable_read_endpoint, function(result) {
+             $.each(result.records, function(key,value) {
+                 items = [];
+                     items.push(value.fields.year);
+                     items.push(value.fields.percentage);
+                     dataSet.push(items);
+                     console.log(items);
+              }); // end .each
+              console.log(dataSet);
+
+           $('#table2').DataTable( {
+               data: dataSet,
+               retrieve: true,
+               columns: [
+                   { title: "year",
+                     defaultContent:""},
+                   { title: "percentage",
+                       defaultContent:"" },
+               ]
+           } );
+
+           var chart = c3.generate({
+                data: {
+                    columns: dataSet,
+                    type : 'bar'
+                },
+                axis: {
+                  x: {label: 'Product'},
+                  y: {label: '# of Items'}
+                },
+                bar: {
+                    title: "# of Items by Product Category:",
+                }
+            });
+
+      }); // end .getJSON
+
+   }); // end button
+
+}); // document ready
